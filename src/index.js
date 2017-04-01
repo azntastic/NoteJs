5731f6709b9626ab2233c0f56e70bd46f5257d29
Ryan <Ryanchu.ws@gmail.com>
2017-04-01 17:21:45 +0100

    got display single note functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 var notelist = new NoteList
-notelist.create("Roar")
-notelist.create("Woof")
-notelist.create("Meow")
+notelist.create("Today for breakfast I had green eggs and ham")
+notelist.create("I must say I prefer ham over spam")
+notelist.create("Unfortunately ham is more spenny than spam")
+notelist.create("So I guess I'll just have to eat more spam")
 
 var notecontroller = new NoteController(notelist)
 
 notecontroller.DisplayNotes(document.getElementById('app'))
+notecontroller.urlChangeDisplaysNotes()
--- src/note_list_view.js
+++ src/note_list_view.js
@@ -12,10 +12,6 @@
     }
     else {
       this.noteListToPrint.printList().forEach(function(element){
-        // var url = window.location.href
-        // url += window.location.href + this.noteListToPrint.list[0].id
-        // console.log(url)
-        // console.log(element.id)
         printAsHTML += "<li><a href=#notes/" + element.id + " id='note-link'>" + element.showText().substring(0,20) + "</a></li>"
       })
     }
